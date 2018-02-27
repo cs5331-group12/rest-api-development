e041edb15c62f158d39a99fb4d3db39c8a42b63f
Low Wei Kit <weikit@u.nus.edu>
2018-02-27 16:58:50 +0800

    updated docker scripts to work with django

--- src/service/start_services.sh
+++ src/service/start_services.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 apachectl start
-python /service/app.py
+python /service/encrypted/manage.py makemigrations
+python /service/encrypted/manage.py migrate
+python /service/encrypted/manage.py runserver 0.0.0.0:8080
