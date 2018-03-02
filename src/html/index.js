d4826bfedc71ea13cb1bcb623c1b4136552bb2f6
joshuaaung <mj_kyawswa@yahoo.com>
2018-03-03 02:46:22 +0800

    updates

--- src/html/index.js
+++ src/html/index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     init_side_nav(".view-public");
     // TODO: get results from backend
     $.ajax({
+        crossDomain: true,
         url:"http://localhost:8080/diary",
         success:function(data) {
           console.log(data)
