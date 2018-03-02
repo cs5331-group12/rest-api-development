b2f4a194f80ee9a4157ce29cfa1af18983db234f
joshuaaung <mj_kyawswa@yahoo.com>
2018-03-03 02:57:43 +0800

    updates

--- src/html/index.js
+++ src/html/index.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
     $.ajax({
         crossDomain: true,
         type: 'GET',
-        dataType: 'jsonp',
         url: "http://localhost:8080/diary",
         success:function(data) {
           console.log(data)
