9f6870c844fdc1ecb3a7205167c444216e4664a7
joshuaaung <mj_kyawswa@yahoo.com>
2018-03-03 03:05:04 +0800

    updates

--- src/service/encrypted/encrypted/settings.py
+++ src/service/encrypted/encrypted/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -41,11 +42,13 @@ INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
     'diary.apps.DiaryConfig',
     'users.apps.UsersConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
