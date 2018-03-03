c92ddf68df4aa9ff9657cabe0ec50f171f514360
joshuaaung <mj_kyawswa@yahoo.com>
2018-03-03 12:22:50 +0800

    updated

--- src/html/index.js
+++ src/html/index.js
@@ -1,36 +1,19 @@
 $(document).ready(function() {
     init_top_nav("Public Diaries");
     init_side_nav(".view-public");
-    // TODO: get results from backend
+
+    var results = {};
+
+    // TODO: get public diaries from backend
     $.ajax({
-        crossDomain: true,
         type: 'GET',
         url: "http://localhost:8080/diary",
         success:function(data) {
-          console.log(data)
+          results = data;
         }
     });
 
-    var results = {
-        "status": true,
-        "result": [
-          {
-            "id": 1,
-            "title": "My First Project",
-            "author": "ashrugged",
-            "publish_date": "2013-02-27T13:37:00+00:00",
-            "public": true,
-            "text": "If you don't know, the thing to do is not to get scared, but to learn."
-          },
-          {
-            "id": 2,
-            "title": "A New Lesson!",
-            "author": "audrey123talks",
-            "publish_date": "2013-02-29T13:37:00+00:00",
-            "public": true,
-            "text": "Check out my latest video!"
-          }
-        ]
-      }
-    init_diaries(results["result"], ".main-section-container", false);
+    if(results["status"]) {
+        init_diaries(results["result"], ".main-section-container", false);
+    }
 });
