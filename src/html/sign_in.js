e1924fcd299eab1447cc8caebd4e117d460513e0
joshuaaung <mj_kyawswa@yahoo.com>
2018-03-03 13:24:57 +0800

    updated

--- src/html/sign_in.js
+++ src/html/sign_in.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
+  var logged_in = false;
+
+  //check if there is stored token
+  var token = sessionStorage.getItem('token');
+
+  if(token) {
     // TODO: Check if user is logged in
-    var logged_in = false;
-    if (logged_in) {
-        window.location.href = "/";
-    }
-    init_top_nav("Sign in");
-    init_side_nav(".auth");
-    init_auth_form(true);
+    $.ajax({
+      type: 'POST',
+      data: {
+        "token": token,
+      },
+      url: "http://localhost:8080/users/expire",
+      success:function(data) {
+        if(data['status']) {
+          logged_in = true;
+        }
+      }
+    });
+  }
+
+  if (logged_in) {
+      window.location.href = "/";
+  }
+
+  init_top_nav("Sign in");
+  init_side_nav(".auth");
+  init_auth_form(logged_in);
 })
