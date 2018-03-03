c54759cb482444f1c8394f0dddf5c80eb3826673
joshuaaung <mj_kyawswa@yahoo.com>
2018-03-03 13:14:49 +0800

    updated

--- src/html/register.js
+++ src/html/register.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
   console.log(token)
   // TODO: Check if user is logged in
   // var logged_in = false || ;
-  if (logged_in) {
-      window.location.href = "/";
-  }
+  // if (logged_in) {
+  //     window.location.href = "/";
+  // }
   init_top_nav("Register");
   init_side_nav(".auth");
   init_auth_form();
