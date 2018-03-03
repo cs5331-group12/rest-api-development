93cab5c553af3d2888ad6981faa45959dfe9cb1c
joshuaaung <mj_kyawswa@yahoo.com>
2018-03-03 13:13:48 +0800

    updated

--- src/html/register.js
+++ src/html/register.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
-    // TODO: Check if user is logged in
-    var logged_in = false;
-    if (logged_in) {
-        window.location.href = "/";
-    }
-    init_top_nav("Register");
-    init_side_nav(".auth");
-    init_auth_form();
+  //check if there is stored token
+  var token = sessionStorage.getItem('key');
+  console.log(token)
+  // TODO: Check if user is logged in
+  // var logged_in = false || ;
+  if (logged_in) {
+      window.location.href = "/";
+  }
+  init_top_nav("Register");
+  init_side_nav(".auth");
+  init_auth_form();
 })
