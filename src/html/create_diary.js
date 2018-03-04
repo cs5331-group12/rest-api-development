1680567e6d24a581985153fb49085a1eaa530cbe
joshuaaung <mj_kyawswa@yahoo.com>
2018-03-04 13:02:52 +0800

    updated

--- src/html/create_diary.js
+++ src/html/create_diary.js
@@ -1,24 +1,5 @@
 $(document).ready(function() {
-    var logged_in = false;
-
-    //check if there is stored token
-    var token = sessionStorage.getItem('token');
-    logged_in = !(token == null || token == "");
-
-    if(logged_in) { //might be logged in. So retrieve the user info to check if this user with this token exist
-        $.ajax({
-          type: 'POST',
-          data: {
-            "token": token,
-          },
-          url: "http://localhost:8080/users",
-          success:function(data) {
-            if(!data['status']) {
-              logged_in = false;
-            }
-          }
-        });
-    }
+    var logged_in = isLoggedIn();
 
     if (!logged_in) {
         window.location.href = "/sign_in.html"
@@ -58,7 +39,7 @@ function submitDiary() {
             "title": "No One Can See This Post",
             "public": false,
             "text": "It is very secret!"
-          }
+          },
           url: url,
           success:function(data) {
             cleanUpForm()
